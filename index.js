64d9f0331c7632a9016a9b629082386bac6b94ee
LazyBunicu <33199156+LazyBunicu@users.noreply.github.com>
2017-10-29 13:52:58 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,237 @@
+const Discord = require("discord.js");
+
+const TOKEN = "MzU4NjI0MDEyNTc1NzAzMDQy.DJ_TFQ.cL8iQSLuqqw1bZqFvKjEItmRK1U"
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+console.log("Ready");
+});
+
+bot.on("message", function(message) {
+ if (message.author.equals(bot.user)) return;
+
+ if (message.content == "salut") {
+     message.channel.sendMessage("Neata");
+ }
+
+  if (message.content == "Salut") {
+      message.channel.sendMessage("Neata");
+  }
+
+
+
+    if (message.content == "Ce Faci?") {
+        message.channel.sendMessage("Bine");
+    }
+
+    if (message.content == "ce faci?") {
+        message.channel.sendMessage("Nasol");
+    }
+        if (message.content == "-ajutor") {
+            message.channel.sendMessage("``` eu nu violez capre, doar fetite smechere```");
+        }
+
+    if (message.content == "Ce faci?") {
+        message.channel.sendMessage("Mai bine decat oricine");
+    }
+    if (message.content == "ce faci") {
+        message.channel.sendMessage("Adica");
+    }
+    if (message.content == "Ce faci") {
+        message.channel.sendMessage("Chestii Dubioase");
+   
+    }
+    if (message.content == "Noapte Buna") {
+        message.channel.sendMessage("Spune-i Anisoarei sa doarma");
+
+    }
+    if (message.content == "noapte buna") {
+        message.channel.sendMessage("Spune-i Anisoarei sa doarma");
+
+    }
+    if (message.content == "Noapte buna") {
+        message.channel.sendMessage("Spune-i Anisoarei sa doarma");
+
+    }
+    if (message.content == "Unde a disparut DeEa") {
+        message.channel.sendMessage("SECRET DE STAT");
+
+    }
+    if (message.content == "Noapte buna") {
+        message.channel.sendMessage("Sa ai o seara faina");
+
+    }
+    if (message.content == "Noapte Buna") {
+        message.channel.sendMessage("Sa ai o seara faina");
+
+    }
+    if (message.content == "noapte buna") {
+        message.channel.sendMessage("Sa ai o seara faina");
+
+    }
+        if (message.content == "Respect Bunicului") {
+            message.channel.sendMessage("RESPECT RESPECT RESPECT");
+
+        }
+                if (message.content == "respect bunicului") {
+                    message.channel.sendMessage("RESPECT RESPECT RESPECT");
+
+                }
+                        if (message.content == "Respect bunicului") {
+                            message.channel.sendMessage("RESPECT RESPECT RESPECT");
+
+                        }
+                                if (message.content == "respect Bunicului") {
+                                    message.channel.sendMessage("RESPECT RESPECT RESPECT");
+
+                                }
+                                        if (message.content == "E Anisoara virgina") {
+                                            message.channel.sendMessage("nu");
+
+                                        }
+                if (message.content == "cine a dezvirginat-o") {
+                    message.channel.sendMessage("Savu si Deny");
+
+                }
+                                if (message.content == "Pot primi rank?") {
+                                    message.channel.sendMessage("Nu. Munceste pentru grad");
+
+                                }
+                             if (message.content == "LazyBot Vezi ca-ti dau cel mai mic rank") {
+                                                                    message.channel.sendMessage("Du-te dracu");
+
+                             } 
+                             if (message.content == "Vezi ca te violez") {
+                                message.channel.sendMessage("Du-te dracu");
+
+}
+if (message.content == "Nu e asa ca deea e sexy?") {
+    message.channel.sendMessage("Ce Mai intrebare");
+
+}
+if (message.content == "Nu e asa ca deea e sexy") {
+    message.channel.sendMessage("Normal");
+
+}
+if (message.content == "e deea sexy?") {
+    message.channel.sendMessage("Normal");
+
+}
+if (message.content == "e deea sexy") {
+    message.channel.sendMessage("Normal");
+
+}
+if (message.content == "E DeEa sexy?") {
+    message.channel.sendMessage("Normal");
+
+}
+if (message.content == "nu e asa ca deea e sexy?") {
+    message.channel.sendMessage("Cum Spui tu?");
+
+}
+if (message.content == "nu e asa ca deea e sexy") {
+    message.channel.sendMessage("De ce ma intrebi pe mine");
+
+}
+if (message.content == "Nu e asa ca DeEa e sexy?") {
+    message.channel.sendMessage("Normal ca da");
+
+}
+if (message.content == "Nu e asa ca DeEa e sexy") {
+    message.channel.sendMessage("Da");
+
+}
+if (message.content == "Nu e asa ca -DeEa- e sexy?") {
+    message.channel.sendMessage("Normal");
+
+}
+if (message.content == "Nu e asa ca -DeEa- e sexy") {
+    message.channel.sendMessage("Da.");
+
+}
+
+if (message.content == "Nu e asa ca deea e sexy") {
+    message.channel.sendMessage("Da");
+
+}
+   
+   
+   
+   
+   
+   
+                    if (message.content == "IDEI NOI") {
+                                      message.channel.sendMessage("Ai o idee noua pentru bot? Scrie un mesaj privat Bunicului Lazy, si poate ideea ta apare intr-o noua actualizare!!! :joy:");
+                                                                }
+                                                                         if (message.content == "cine a mai calarit-o pe Anisoara") {
+                                                                        message.channel.sendMessage("Victor! :heart: :joy:");
+
+                                                                         }
+                                                                         if (message.content == "LazyBot esti virgin?") {
+                                                                          message.channel.sendMessage("Nu");
+
+                                                                            }
+                                                                            if (message.content == "LazyBot ce parere ai despre Savu?") {
+                                                                                message.channel.sendMessage("PROTESTAM");
+
+                                                                            }
+                                                                            if (message.content == "LazyBot ce parere ai despre savu?") {
+                                                                                message.channel.sendMessage("PROTESTAM");
+
+                                                                               }
+                                                                            if (message.content == "LazyBot ce parere ai despre Savu") {
+                                                                                message.channel.sendMessage("PROTESTAM");
+                                                                 }
+                                                               if (message.content == "LazyBot ce parere ai despre savu") {
+                                                                    message.channel.sendMessage("PROTESTAM");
+         
+                                                                            }
+if (message.content == "Este violenta permisa?") {
+    message.channel.sendMessage("Da, pot sa-ti dau eu un pumn","Tu cum crezi?");
+
+}
+if (message.content == "este violenta permisa?") {
+    message.channel.sendMessage("Da, pot sa-ti dau eu un pumn","Tu cum crezi?");
+
+}
+if (message.content == "Este violenta permisa") {
+    message.channel.sendMessage("Da, pot sa-ti dau eu un pumn","Tu cum crezi?");
+
+}
+if (message.content == "este violenta permisa") {
+    message.channel.sendMessage("Da, pot sa-ti dau eu un pumn","Tu cum crezi?");
+
+}
+ if (message.content == "Unde a disparut Savu?") {
+     message.channel.sendMessage("SECRET DE STAT");
+
+ }
+  if (message.content == "Cate ai violat pana acum") {
+      message.channel.sendMessage("Atatea cati ani are tot arborele familiei tale");
+
+  }
+  if (message.content == "cate ai violat pana acum") {
+    message.channel.sendMessage("Atatea cati ani are tot arborele familiei tale");
+
+}
+
+if (message.content == "esti virgin?") {
+    message.channel.sendMessage("Nu");
+
+}
+if (message.content == "esti virgin") {
+    message.channel.sendMessage("Nu");
+
+}
+if (message.content == "Esti virgin") {
+    message.channel.sendMessage("Nu");
+
+}
+if (message.content == ".") {
+    message.channel.sendMessage("Manca-ti-as gurita. vorbeste normal");
+
+}
+});
+
+
+bot.login(TOKEN);
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Get username
+
+?>
+<<html>
+<head>
+<title>Chatter</title>
+<link rel='stylesheet' type='text/css' href='css/style.css' />
+
+</head>
+</html>
